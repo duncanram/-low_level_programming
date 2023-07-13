7852119939257d4d34f934e1db41f11078e40a4d
duncanram <duncan.ram@gmail.com>
2023-07-14 00:28:53 +0200

    im awake

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include "main.h"
 
-
 /**
  * *_memset - fills memory with a constant byte
  * @s: memory area to be filled
@@ -12,16 +11,14 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-        unsigned int i;
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
 
-        for (i = 0; i < n; i++)
-        {
-                s[i] = b;
-        }
-
-
-        return (s);
+	return (s);
 }
 
 
@@ -34,22 +31,18 @@ char *_memset(char *s, char b, unsigned int n)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-        char *ptr;
+	char *ptr;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-        if (nmemb == 0 || size == 0)
-                return (NULL);
+	ptr = malloc(size * nmemb);
 
+	if (ptr == NULL)
+		return (NULL);
 
-        ptr = malloc(size * nmemb);
+	_memset(ptr, 0, nmemb * size);
 
-
-        if (ptr == NULL)
-                return (NULL);
-
-
-        _memset(ptr, 0, nmemb * size);
-
-
-        return (ptr);
+	return (ptr);
 }
+
