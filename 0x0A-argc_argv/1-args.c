a5073492fba8ca68f26e56f735b07a943e81d34a
duncanram <94283243+duncanram@users.noreply.github.com>
2023-07-17 01:13:00 +0200

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -13,9 +13,10 @@
 
 int main(int argc, char *argv[])
 {
-(void) argv; /*Ignore argv*/
-        printf("%d\n", argc - 1);
+	(void) argv; /*Ignore argv*/
+	printf("%d\n", argc - 1);
 
 
-        return (0);
+	return (0);
 }
+
