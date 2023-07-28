b58b077ce522d965d5ba213f9aa98c6030b337d7
duncanram <duncan.ram@gmail.com>
2023-07-28 14:23:00 -0700

    we on

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * first - prints sentence before main func is executed
+ */
+
+void first(void) __attribute__ ((constructor));
+
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
