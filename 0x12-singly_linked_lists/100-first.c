cfcae5ea3ef00d9f8ae3f5763654090d0d2ab995
duncanram <duncan.ram@gmail.com>
2023-07-28 14:35:42 -0700

    we on

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
- * first - prints sentence before main func is executed
+ * first - prints a sentence before the main function is executed
  */
 
 void first(void) __attribute__ ((constructor));
-
-
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
